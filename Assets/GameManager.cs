db2622d2cf5cd40d64676da3dd4e6937017e088f
BonghoCha <37327313+BonghoCha@users.noreply.github.com>
2021-08-27 01:49:50 +0900

    [Add] 초기 프로젝트 추가

---
+++ Assets/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("===Monster Setting===")]
+    [SerializeField] GameObject[] MonsterPrefabs;
+    [SerializeField] Vector3 SpawnPoints;
+
+    [SerializeField] Transform[] MovePoints;
+
+    public static GameManager instance = null;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+
+            // 중복된 인스턴스 제거
+            GameManager[] inst = FindObjectsOfType<GameManager>();
+            foreach (GameManager i in inst)
+            {
+                Destroy(i.gameObject);
+            }
+
+            DontDestroyOnLoad(this);
+        }
+    }
+
+    public Vector3 GetSpawnPoint()
+    {
+        return MovePoints[0].localPosition;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/MonsterController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MonsterController : MonoBehaviour
+{
+    enum Type
+    {
+        Slime,
+        TurtleShell,
+        Beholder,
+        Chest
+    }
+
+    [Header("===Monster Properties===")]
+    [SerializeField] Type type;
+
+    [SerializeField] float hp;
+    [SerializeField] float power;
+    [SerializeField] float shield;
+    [SerializeField] float speed;
+
+    Vector3 SpawnPoint;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnPoint = GameManager.instance.GetSpawnPoint();
+
+        transform.localPosition = SpawnPoint;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
